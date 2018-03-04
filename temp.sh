7b545a70236c4579d770d3f22bb0ccc765908e78
vipin-bhardwaj <vipinbhardwaj@gmail.com>
2018-03-04 19:57:32 +0530

    first commit

---
+++ temp.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install libxine2-dev libv4l-dev
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install libvorbis-dev libxvidcore-dev
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install x264 v4l-utils
+
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+pip install numpy scipy matplotlib scikit-image scikit-learn ipython
